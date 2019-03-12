1f8fcbd128fcf743e79735b6c6ff3123a83d7080
Daniel Thor Thorisson <danielthorr@gmail.com>
2019-03-12 12:26:00 +0000

    Completed a part of of skilaverkefni3

--- Verkefni3/Skilaverkefni_3a.sql
+++ Verkefni3/Skilaverkefni_3a.sql
@@ -30,7 +30,8 @@ inner join nemendaskraning n2
 -- sækið númer og heiti allra áfanga sem byrja á bókstafnum E
 -- hér má nota annað hvort áfanga númerið eða áfanga heitið!
 
-
+select afangaNumer, afangaHeiti from afangar
+where afangaNumer like "E%";
 
 -- 6:
 -- Gefnir eru eftirfarandi áfangar sem kenndir eru í rafiðnaðarskólanum
@@ -40,17 +41,34 @@ inner join nemendaskraning n2
 -- RAFM1GA05AR - Rafmagnsfræði 1	önn 2
 -- Áfangarnir eru 5 einingar nema Skyndihjálp sem gefur eina einingu.
 
+
 -- 6a:
 -- skráið þessa áfanga í gagnagrunninn.
+
+insert into afangar(afangaNumer, afangaHeiti, einingar)values("SKYN2SE01AT", "Skyndihjálp", 1);
+
+insert into afangar(afangaNumer, fangaHeiti)
+values 
+("RAFL1GA03AR", "Raflagnir 1"), 
+("RAFL1GA03BR", "Raflagnir 2"), 
+("RAFM1GA05AR", "Rafmagnsfræði 1");
 
 -- 6b:
 -- Þessir áfagnar eru kenndir í grunnnámi rafiðna sem er braut innan Raftækniskólans.
 -- Skráðið þessa braut í grunninn.
 
+insert into brautir(heitiBrautar, tilheyrir)values("Grunnám rafiðna", 6);
+
 -- 6c:
 -- Setjið þessa áfanga í framboð á nýju brautinni(Gunnnám rafiðna) með því að skrá 
 -- viðeigandi upplýsingar í töfluna Afangaframboð.
 
+insert into afangaframbod(brautarNumer, afangaNumer, onnAfanga, skylda)
+values
+(10, "SKYN2SE01AT", 1, true),
+(10, "RAFL1GA03AR", 1, true),
+(10, "RAFL1GA03BR", 2, true),
+(10, "RAFM1GA05AR", 2, true);
 
 
 -- 7:
@@ -60,19 +78,30 @@ inner join nemendaskraning n2
 -- MENL1AL05AT
 -- STÆR4HT05FT
 
-
+select * from afangar
+where afangaNumer not in ("DANS2BM05AT", "ÍÞRÓ1GH02AT", "MENL1AL05AT", "STÆR4HT05FT");
 
 -- 8:
--- veljið meðaleinkunn á hvern áfanga. Birtið áhanganúmerið ásamt meðaleinkunn:
+-- veljið meðaleinkunn á hvern áfanga. Birtið áfanganúmerið ásamt meðaleinkunn:
 
-
+select afangar.afangaNumer as "Númer áfanga", afangaHeiti as "Áfangaheiti", 
+(
+    select avg(einkunn) from nemendaskraning 
+    where nemendaskraning.afangaNumer = afangar.afangaNumer
+) as "Meðaleinkunn"
+from afangar;
 
 -- 9:
 -- Veljð nú meðaleinkunn hvers nemanda(nemandiID) og rúnnið einkunnina af uppá einn aukastaf( td: 8.5000 verður 9.0 en 8.4000 verður 8.0)
 -- Takið aðeins einkunnir +afanga sem er lokið(ekki null)
 -- https://www.w3schools.com/sql/func_mysql_round.asp
 
-
+select distinct 
+nemandi, fornafn, round(avg(einkunn))
+    from nemendur n1
+inner join nemendaskraning n2 
+    on n1.nemandi = n2.nemandiID
+        where n2.einkunn is null;
 
 -- 10:
 -- Hvað eru margir skylduáfangar áætlaðir á hverja önn á tölvubraut(brautarnúmer 7). 
