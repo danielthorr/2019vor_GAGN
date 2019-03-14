b3f5653c1f856d07af11e473825e184e959e4946
Daniel Thor Thorisson <danielthorr@gmail.com>
2019-03-14 07:51:08 +0000

    Completed more of verkefni3

--- Verkefni3/Skilaverkefni_3b.sql
+++ Verkefni3/Skilaverkefni_3b.sql
@@ -28,13 +28,24 @@ order by onnAfanga;
 -- birtið nemanda númer(ID),fornafn og eftirnafn.
 -- ATH: Hér þarf að nota subquery
 
+    -- Ég skil ekki hvar ég á að nota subquery, hér er mín útfærsla án þess að nota subquery
+select nemandiID, fornafn, eftirnafn from nemendaskraning n1
+inner join nemendur n2 on n1.nemandiID = n2.nemandi
+where n1.afangaNumer = "STÆR2BR05AT";
 
+        -- Tilraun til þess að setja subquery inn í þetta
+    select nemandiID, fornafn, eftirnafn from nemendaskraning n1
+    inner join nemendur n2 on n1.nemandiID = n2.nemandi
+    where n1.afangaNumer = ( select afangaNumer from afangar where afangaNumer = "STÆR2BR05AT" )
 
 -- 4:
 -- Sækið allar eikunnir nemenda sem eru lægri en meðaleiknunn(allra áfanga)
 -- birtið nemanda ID, áfanganúmer og einkunn
 
-
+select nemandiID, afangaNumer, einkunn from nemendaskraning
+where einkunn is not null 
+and  einkunn < ( select avg(einkunn) from nemendaskraning 
+                where einkunn is not null )
 
 -- 5:
 -- Sækið alla áfanga sem eru í framboði á brautum skólans.
@@ -42,3 +53,7 @@ order by onnAfanga;
 -- Gæti verið kúl að raða eftir undirskólum og svo á hvaða önn áfangarnir eru!
 -- ATH: Töflur sem þarf að nota eru: Undirskolar, Brautir, Afangaframboð.
 
+    -- WIP - ekki búið að klára
+select afangaNumer, onnAfanga, heitiBrautar, heiti from afangaframbod
+inner join brautir using(brautarNumer)
+inner join undirskolar on undirskolar.undirskolaNumer = brautir.tilheyrir;
